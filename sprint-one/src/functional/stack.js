02ca687b3ee8634bfbd1dc8638a9957b2dbc989f
msmith9393 <megan@tokaypress.com>
2016-03-02 14:46:33 -0800

    Reports a size of 0 after removing more items than were added

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,22 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++;
   };
 
   someInstance.pop = function() {
+    if(count > 0){
+      count--;
+    }
+    
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
