6d6c8bd4571dad2b4f738f35b2064941fcd62df3
msmith9393 <megan@tokaypress.com>
2016-03-02 16:15:33 -0800

    Complete queue test allows sequentially adding and removing items

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function() {
+    if ( count > 0 ) {
+      count--;
+    }
+    for (var i = 1; i <= count + 1; i++) {
+      
+      var dequeued = storage[i];
+    
+      delete storage[i];
+      return dequeued;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
