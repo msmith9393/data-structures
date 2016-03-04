1164c30e9b3dbfa809feedf6e45e2677bce29f39
msmith9393 <megan@tokaypress.com>
2016-03-04 11:13:28 -0800

    Finished graph data structure

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -9,7 +9,7 @@ var Graph = function() {
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.nodes[node] = {value: node, edges: []};  
+  this.nodes[node] = {value: node, edges: {}};  
 };
 
 // ------------------------
@@ -30,7 +30,7 @@ Graph.prototype.removeNode = function(node) {
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  if (this.nodes[fromNode].edges.indexOf(toNode) > -1) {
+  if (this.nodes[fromNode].edges[toNode]) {
     return true;
   }
   return false;
@@ -39,23 +39,34 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.nodes[fromNode].edges.push(toNode);
-  this.nodes[toNode].edges.push(fromNode);
+  this.nodes[fromNode].edges[toNode] = true;
+  this.nodes[toNode].edges[fromNode] = true;
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  
+  delete this.nodes[fromNode].edges[toNode];
+  delete this.nodes[toNode].edges[fromNode];
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var node in this.nodes) {
+    cb(node);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addNode = O(1) constant time operation.
+ contains = O(1) constant time operation.
+ removeNode = O(1) constant time operation.
+ hasEdge = O(1) constant time operation.
+ addEdge = O(1) constant time operation.
+ removeEdge = O(1) constant time operation.
+ forEachNode = O(n) linear time operation.
  */
 
 
