e1049a1903ca1219963b6f065d584b33d6a46dd8
msmith9393 <megan@tokaypress.com>
2016-03-04 11:46:58 -0800

    Finish set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,23 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage === undefined) {
+    this._storage = {};
+  }
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item); 
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+add: O(1) - constant time
+contains: O(1) - constant time
+remove: O(1) - constant time
  */
