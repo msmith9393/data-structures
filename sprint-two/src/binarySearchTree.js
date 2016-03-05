4f24aa2516c5485b063ec0536d00ec8ce65d7afd
msmith9393 <megan@tokaypress.com>
2016-03-05 11:02:00 -0800

    Complete Binary Search Tree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -49,9 +49,20 @@ BinarySearchTree.prototype.contains = function(value, currentNode) {
 
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
 
-}; 
+  callback(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+
+};
+
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert and contains both have O(log n) time complexity.
+ depthFirstLog has linear O(n) time complexity
  */
 
 
