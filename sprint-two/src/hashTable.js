80eef60a3f6b90a971956bc7635b86e2b7f4e663
msmith9393 <megan@tokaypress.com>
2016-03-04 16:26:49 -0800

    Complete hash table retrieve function

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,21 +7,38 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage.get(index) === undefined) {
+  var bucket = this._storage.get(index);
+  if (bucket === undefined) {
     this._storage.set(index, [[k, v]]);      
   } else {
-    this._storage.get(index).push([k, v]);
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+      } else {
+        this._storage.get(index).push([k, v]);
+      }
+    }
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  if (bucket !== undefined) {
+    for ( var i = 0; i < bucket.length; i++ ) {
+      if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  var bucket = this._storage.get(index);
+  
+
+  // this._storage.set(index, undefined);
 
 };
 
